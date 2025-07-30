f2af4fb0f37c6c7e09703a9ae1849c067f00f3d1
Esa Koskinen <esakoskin2@gmail.com>
2025-07-31 02:51:16 +0900

    refactor: set up CSS and new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import { ThemeProvider } from '@/components/theme-provider'
 import { Button } from './components/ui/button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { AreaChartExample } from './components/game/area-chart-example'
+import { Progress } from './components/ui/progress'
+import { ChartAreaGradient } from './components/game/chart-area-gradient'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => (prev >= 100 ? 0 : prev + 10))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <Button onClick={() => setCount(count + 1)}>Click me count is {count}</Button>
-        <Button variant="destructive">Buttons</Button>
+        <Progress value={progress} className="w-64 mb-4" />
+        <Button
+          onClick={() => {
+            setCount(count + 1)
+            setProgress(progress + 10)
+          }}
+        >
+          Click me count is {count}
+        </Button>
+        <Button variant="ghost">Buttons</Button>
       </div>
+      <AreaChartExample />
+      <ChartAreaGradient />
     </ThemeProvider>
   )
 }
