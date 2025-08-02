9d13181b8fc8fdf25ebd50b3a05cf9ef721dfacb
IvarK <ivarkera@gmail.com>
2025-08-03 02:19:51 +0300

    make delete save button require multiple clicks

--- src/components/game/screens/game-screen.tsx
+++ src/components/game/screens/game-screen.tsx
@@ -3,8 +3,36 @@ import { SidePanel } from '../side-panel'
 import { Button } from '@/components/ui/button'
 import { clearSave } from '@/lib/saving'
 import { ActionCards } from '../action-cards'
+import { useState } from 'react'
 
 export function GameScreen() {
+  const [clickCount, setClickCount] = useState(0)
+
+  const handleSaveDelete = () => {
+    setClickCount((prev) => prev + 1)
+    if (clickCount >= 4) {
+      clearSave()
+      setClickCount(0)
+    }
+  }
+
+  const deleteButtonText = (() => {
+    switch (clickCount) {
+      case 0:
+        return 'Delete save'
+      case 1:
+        return 'Are you sure?'
+      case 2:
+        return 'Really sure?'
+      case 3:
+        return 'Last chance!'
+      case 4:
+        return 'Clicking now will delete your save!'
+      default:
+        return '???'
+    }
+  })()
+
   return (
     <>
       <div className="grid grid-cols-[3fr_1fr]">
@@ -16,8 +44,8 @@ export function GameScreen() {
           <SidePanel />
         </div>
       </div>
-      <Button variant="destructive" className="fixed bottom-4" onClick={() => clearSave()}>
-        Delete save
+      <Button variant="destructive" className="fixed bottom-4" onClick={handleSaveDelete}>
+        {deleteButtonText}
       </Button>
       {/* <ThemeModeToggle /> */}
     </>
