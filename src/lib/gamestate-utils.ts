c509bcafefc8a22c32b3f9ef00c7e707ebac38c6
Esa Koskinen <esakoskin2@gmail.com>
2025-08-03 15:35:12 +0900

    feat: add a debug initial game state for easier testing

--- src/lib/gamestate-utils.ts
+++ src/lib/gamestate-utils.ts
@@ -1,7 +1,7 @@
 import { initialLevelDefinitions } from './data/level-definitions'
 import type { GameState } from './types'
 
-export const initialGameState: GameState = {
+const baseInitialGameState: GameState = {
   generation: 1,
   currentScreen: 'main-menu',
   currentLevel: 'amoeba',
@@ -15,3 +15,75 @@ export const initialGameState: GameState = {
     valueHistory: true,
   },
 }
+
+// - @ts-expect-error
+const debugInitialGameState: GameState = {
+  ...baseInitialGameState,
+  currentScreen: 'rebirth',
+  currentLevel: 'multicellular',
+  levels: {
+    amoeba: {
+      ...baseInitialGameState.levels.amoeba,
+      resourceRecords: {
+        food: 20,
+        energy: 20,
+        nutrients: 20,
+        divisions: 3,
+      },
+      unlocked: true,
+    },
+    multicellular: {
+      ...baseInitialGameState.levels.multicellular,
+      resourceRecords: {
+        food: 800,
+        nutrients: 800,
+        energy: 800,
+        efficiency: 4,
+        'food multiplier': 15,
+        cells: 1400,
+      },
+      unlocked: true,
+    },
+    algae: {
+      ...baseInitialGameState.levels.algae,
+      resourceRecords: {
+        millimeters: 1000,
+        branches: 15,
+        chlorophyll: 500,
+        energy: 2000,
+        hardness: 1000,
+      },
+      unlocked: true,
+    },
+    insect: {
+      ...baseInitialGameState.levels.insect,
+      resourceRecords: {
+        food: 500,
+        energy: 1000,
+        speed: 100,
+        perception: 50,
+        digestion: 100,
+        pheromones: 10,
+        mates: 300,
+        eggs: 10000,
+      },
+      unlocked: true,
+    },
+    crustacean: {
+      ...baseInitialGameState.levels.crustacean,
+      resourceRecords: {
+        energy: 2000,
+        targets: 100,
+        food: 1000,
+        strength: 100,
+        dexterity: 80,
+        vitality: 200,
+        intelligence: 100,
+        mass: 1000,
+      },
+      unlocked: true,
+    },
+  },
+}
+
+export const initialGameState: GameState = debugInitialGameState
